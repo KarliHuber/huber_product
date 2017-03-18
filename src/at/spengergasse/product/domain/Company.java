0a54436fbf49d438b215787d2a20b1adae03fe4b
eis17841 <eis17841@spengergasse.at>
2017-03-18 19:47:04 +0100

    Product

---
+++ src/at/spengergasse/product/domain/Company.java
@@ -0,0 +1,27 @@
+package at.spengergasse.company.domain
+
+import java.util.*
+
+public class Company
+{
+	//properties
+	privat String name = "Coca Cola";
+	
+	//get
+	public String getName()
+	{
+		return name;
+	}
+	
+	//set
+	public void setName(String name)
+	{
+		this.name = name;
+	}	
+	
+	//constructor
+	public Company()
+	{
+		
+	}
+}
---
+++ src/at/spengergasse/product/domain/Product.java
@@ -0,0 +1,15 @@
+package at.spengergasse.product.domain
+
+import java.util.*
+
+public class Product()
+{
+	//properties
+	public ArrayList products;
+	
+	//constructor
+	public Product()
+	{
+		products = new ArrayList();
+	}
+}
