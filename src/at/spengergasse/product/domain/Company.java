394d6c7b064d05e2c92a2946070b690bec3bb5eb
KarliHuber <ferro@gmx.at>
2017-03-19 13:05:35 +0100

    AddCompany

---
+++ src/at/spengergasse/product/domain/Company.java
@@ -0,0 +1,28 @@
+package at.spengergasse.company.domain
+
+import java.util.*
+
+//Übung GITHUB
+public class Company
+{
+	//properties
+	privat String name = "Coca Cola";
+	
+	//get
+	public String getName()
+	{
+		return name;
+	}
+	
+	//set
+	public void setName(String name)
+	{
+		this.name = name;
+	}	
+	
+	//constructor
+	public Company()
+	{
+	
+	}
+}
